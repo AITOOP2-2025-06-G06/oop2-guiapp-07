a231d4f9aec164c82ef0283cf9e2a9d0e767c338
浅野僚介 <ruidora1529@gmail.com>
2025-11-20 14:35:04 +0900

    画像の受け取り・加工、保存機能の追加

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.lecture05_01 import lecture05_01
 from src.lecture05_01_x24099 import lecture05_01_x24099
+from src.gazou import image_edit
 
 if __name__ == "__main__":
     # lecture05_01()
-    lecture05_01_x24099()
+    image_edit()
