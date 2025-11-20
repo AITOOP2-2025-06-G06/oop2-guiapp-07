79b542ed657ba04a6af63d9c01939f79fa695122
shuta1123 <shutay1123@yahoo.co.jp>
2025-11-20 14:42:10 +0900

    コンフリクト前に戻す

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 
-from src.startView import startView
+from src.sampleGUI import CameraWindow
+from PySide6.QtWidgets import QApplication
+import sys
 
 if __name__ == "__main__":
     # lecture05_01()
-    startView()
+    app = QApplication(sys.argv)
+    win = CameraWindow()
+    win.show()
+    sys.exit(app.exec())
